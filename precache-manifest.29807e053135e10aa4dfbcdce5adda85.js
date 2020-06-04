b82da6936cd16d8f2f4591de5e4b2ecb9fe4174f
KENDRY USUARIO DANIEL <kendrysotomorales@gmail.com>
2020-06-03 23:11:30 -0500

    Updates

---
+++ precache-manifest.29807e053135e10aa4dfbcdce5adda85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67556848aaa15e96f88f36d21aed4ea7",
+    "url": "/Pizza-shop/index.html"
+  },
+  {
+    "revision": "4209cd9a4526343e7505",
+    "url": "/Pizza-shop/static/css/main.7eb10bd5.chunk.css"
+  },
+  {
+    "revision": "ee3ba8d539437b5533d6",
+    "url": "/Pizza-shop/static/js/2.597146d3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Pizza-shop/static/js/2.597146d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4209cd9a4526343e7505",
+    "url": "/Pizza-shop/static/js/main.f2034246.chunk.js"
+  },
+  {
+    "revision": "e987719650a501b65b5c",
+    "url": "/Pizza-shop/static/js/runtime-main.e82d1a61.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Pizza-shop/precache-manifest.29807e053135e10aa4dfbcdce5adda85.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Pizza-shop/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
